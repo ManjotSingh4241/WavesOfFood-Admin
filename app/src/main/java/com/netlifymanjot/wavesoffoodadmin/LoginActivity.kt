639d9cd8f379819751575b35adc14531eb679959
Jot <manjotsinghpnp@gmail.com>
2024-09-08 03:36:07 -0600

    Day1.1: little updation in login and signup section

--- app/src/main/java/com/netlifymanjot/wavesoffoodadmin/LoginActivity.kt
+++ app/src/main/java/com/netlifymanjot/wavesoffoodadmin/LoginActivity.kt
@@ -1,17 +1,30 @@
 package com.netlifymanjot.wavesoffoodadmin
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.netlifymanjot.wavesoffoodadmin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.loginbutton.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.donthavebutton.setOnClickListener{
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
